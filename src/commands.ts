cb95bf57e0c9439019ad912a5d045313ad502a54
KM127PL <km127pl2@gmail.com>
2022-12-26 19:05:00 +0100

    Fix: Linter issues

--- src/commands.ts
+++ src/commands.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-var-requires */
 import { Client, Routes, SlashCommandBuilder } from "discord.js";
 import { REST } from "@discordjs/rest";
